b2625da00845b8bd2bf0724ecfad532252a81851
Rohan Jain <rohanj.0606@gmail.com>
2024-09-17 21:12:00 -0500

    added sorting logic

--- pages/react/searchstax-accelerator-react/src/App.tsx
+++ pages/react/searchstax-accelerator-react/src/App.tsx
@@ -1,126 +1,3 @@
-// import React, { useState } from "react";
-// import "./App.scss";
-// import {
-//   SearchstaxWrapper,
-//   SearchstaxInputWidget,
-//   SearchstaxResultWidget,
-//   SearchstaxPaginationWidget,
-//   SearchstaxFacetsWidget,
-// } from "@searchstax-inc/searchstudio-ux-react";
-// import { Searchstax } from "@searchstax-inc/searchstudio-ux-js";
-// import {
-//   ISearchObject,
-//   ISearchstaxParsedResult,
-//   ISearchstaxSuggestProps,
-//   ISearchstaxSuggestResponse,
-// } from "@searchstax-inc/searchstudio-ux-js";
-// import { InputTemplate } from "./templates/inputTemplates";
-// import {
-//   noResultTemplate,
-//   resultsTemplate,
-// } from "./templates/resultsTemplates";
-
-// //@ts-ignore
-// import { config, renderConfig } from "../config.js";
-// import { paginationTemplate } from "./templates/paginationTemplates.js";
-// import {
-//   facetsTemplateDesktop,
-//   facetsTemplateMobile,
-// } from "./templates/facetTemplates.js";
-
-// function App() {
-//   const [searchstaxInstance, setSearchstaxInstance] =
-//     useState<Searchstax | null>(null);
-
-//   const sessionId = generateSessionId(25);
-
-//   function generateSessionId(length: number): string {
-//     let result = "";
-//     const characters =
-//       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-//     for (let i = 0; i < length; i++) {
-//       result += characters.charAt(
-//         Math.floor(Math.random() * characters.length)
-//       );
-//     }
-//     return result;
-//   }
-
-//   function beforeSearch(props: ISearchObject) {
-//     const propsCopy = { ...props };
-//     return propsCopy;
-//   }
-//   function afterSearch(results: ISearchstaxParsedResult[]) {
-//     const copy = [...results];
-//     return copy;
-//   }
-//   function initialized(searchstax: Searchstax) {
-//     setSearchstaxInstance(searchstax);
-//   }
-
-//   function afterAutosuggest(
-//     result: ISearchstaxSuggestResponse
-//   ): ISearchstaxSuggestResponse {
-//     // Modify and return the autosuggest result
-//     return { ...result };
-//   }
-
-//   function beforeAutosuggest(
-//     props: ISearchstaxSuggestProps
-//   ): ISearchstaxSuggestProps | null {
-//     // Modify and return the autosuggest props
-//     return { ...props };
-//   }
-
-//   function afterLinkClick(
-//     result: ISearchstaxParsedResult
-//   ): ISearchstaxParsedResult {
-//     // Modify and return the clicked result
-//     return { ...result };
-//   }
-
-//   return (
-//     <SearchstaxWrapper
-//       searchURL={config.searchURL}
-//       suggesterURL={config.suggesterURL}
-//       trackApiKey={config.trackApiKey}
-//       searchAuth={config.searchAuth}
-//       beforeSearch={beforeSearch}
-//       afterSearch={afterSearch}
-//       authType={"token"}
-//       language={config.language}
-//       initialized={initialized}
-//     >
-//       <div className="searchstax-page-layout-container">
-//         <SearchstaxInputWidget
-//           inputTemplate={InputTemplate}
-//           suggestAfterMinChars={renderConfig.inputWidget.suggestAfterMinChars}
-//           afterAutosuggest={afterAutosuggest}
-//           beforeAutosuggest={beforeAutosuggest}
-//         ></SearchstaxInputWidget>
-//         {/* <SearchstaxFacetsWidget
-//           facetingType="and"
-//           itemsPerPageDesktop={2}
-//           itemsPerPageMobile={3}
-//           specificFacets={undefined}
-//           facetsTemplateDesktop={facetsTemplateDesktop}
-//           facetsTemplateMobile={facetsTemplateMobile}
-//         /> */}
-//         <SearchstaxResultWidget
-//           afterLinkClick={afterLinkClick}
-//           resultsPerPage={10}
-//           renderMethod={"pagination"}
-//           resultsTemplate={resultsTemplate}
-//           noResultTemplate={noResultTemplate}
-//         />
-//         <SearchstaxPaginationWidget paginationTemplate={paginationTemplate} />
-//       </div>
-//     </SearchstaxWrapper>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from "react";
 import "./App.scss";
 import {
@@ -129,14 +6,9 @@ import {
   SearchstaxResultWidget,
   SearchstaxPaginationWidget,
   SearchstaxFacetsWidget,
+  SearchstaxSortingWidget, // Sorting widget
 } from "@searchstax-inc/searchstudio-ux-react";
 import { Searchstax } from "@searchstax-inc/searchstudio-ux-js";
-import {
-  ISearchObject,
-  ISearchstaxParsedResult,
-  ISearchstaxSuggestProps,
-  ISearchstaxSuggestResponse,
-} from "@searchstax-inc/searchstudio-ux-js";
 import { InputTemplate } from "./templates/inputTemplates";
 import {
   noResultTemplate,
@@ -150,6 +22,8 @@ import {
   facetsTemplateDesktop,
   facetsTemplateMobile,
 } from "./templates/facetTemplates.js";
+
+import { searchSortingTemplate } from "./templates/searchSortingTemplate.js";
 
 function App() {
   const [searchstaxInstance, setSearchstaxInstance] =
@@ -169,37 +43,18 @@ function App() {
     return result;
   }
 
-  function beforeSearch(props: ISearchObject) {
+  function beforeSearch(props) {
     const propsCopy = { ...props };
     return propsCopy;
   }
-  function afterSearch(results: ISearchstaxParsedResult[]) {
+
+  function afterSearch(results) {
     const copy = [...results];
     return copy;
   }
-  function initialized(searchstax: Searchstax) {
+
+  function initialized(searchstax) {
     setSearchstaxInstance(searchstax);
-  }
-
-  function afterAutosuggest(
-    result: ISearchstaxSuggestResponse
-  ): ISearchstaxSuggestResponse {
-    // Modify and return the autosuggest result
-    return { ...result };
-  }
-
-  function beforeAutosuggest(
-    props: ISearchstaxSuggestProps
-  ): ISearchstaxSuggestProps | null {
-    // Modify and return the autosuggest props
-    return { ...props };
-  }
-
-  function afterLinkClick(
-    result: ISearchstaxParsedResult
-  ): ISearchstaxParsedResult {
-    // Modify and return the clicked result
-    return { ...result };
   }
 
   return (
@@ -215,31 +70,34 @@ function App() {
       initialized={initialized}
     >
       <div className="searchstax-page-layout-container">
-        {/* First row: Search bar centered */}
+        {/* First row: Search bar */}
         <div className="search-bar-row">
           <SearchstaxInputWidget
             inputTemplate={InputTemplate}
             suggestAfterMinChars={renderConfig.inputWidget.suggestAfterMinChars}
-            afterAutosuggest={afterAutosuggest}
-            beforeAutosuggest={beforeAutosuggest}
           />
         </div>
 
-        {/* Second row: Filter on the left and results on the right */}
+        {/* Second row: Sorting widget */}
+        <div className="sorting-row">
+          <SearchstaxSortingWidget
+            searchSortingTemplate={searchSortingTemplate}
+          />
+        </div>
+
+        {/* Third row: Filter and results */}
         <div className="search-layout-row">
           <div className="filter-column">
             <SearchstaxFacetsWidget
               facetingType="or"
               itemsPerPageDesktop={3}
               itemsPerPageMobile={2}
-              specificFacets={undefined}
               facetsTemplateDesktop={facetsTemplateDesktop}
               facetsTemplateMobile={facetsTemplateMobile}
             />
           </div>
           <div className="result-column">
             <SearchstaxResultWidget
-              afterLinkClick={afterLinkClick}
               resultsPerPage={10}
               renderMethod={"pagination"}
               resultsTemplate={resultsTemplate}
